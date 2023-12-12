62c694498d900ce985d6e607a81ce118e549bf9f
aseahjh <102alvin@gmail.com>
2023-12-13 02:13:45 +0800

    added root bookstack icon

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import tkinter 
 from tkinter import *
 
-root=Tk()
+from simpleicons.icons import si_bookstack
+from simpleicons.image import icon_to_image
+from PIL import Image, ImageTk
+
+root = Tk()
 root.title("To-Do-List")
+root.geometry("400x650+400+100")
+
+task_list = []
+
+#Root Image Icon
+xml_bytes = si_bookstack.get_xml_bytes(fill="black")
+img = icon_to_image(xml_bytes, bg=0xFFFFFF, scale=(5, 5))
+img = img.resize((32, 32), Image.ANTIALIAS)
+task_icon = ImageTk.PhotoImage(img)
+root.iconphoto(False, task_icon)
+
+root.mainloop()
